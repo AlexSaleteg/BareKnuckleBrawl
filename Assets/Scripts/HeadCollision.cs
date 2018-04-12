657b6ad7abb3924e0db9da639a218bfc27f2ac07
Unknown <fireblaze123@gmail.com>
2018-04-12 15:44:13 +0200

    hitting now works

---
+++ Assets/Scripts/HeadCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadCollision : MonoBehaviour {
+
+    public string player;
+
+    private TakeDamage damage;
+    void Start()
+    {
+        damage = GameObject.Find(player).GetComponent<TakeDamage>();
+    }
+
+   
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "fist")
+        {
+            damage.InflictDamage(1);
+        }
+    }
+}
