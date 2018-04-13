8c7b9123593c3dda8a28ad427bdd134d63eb33fb
Unknown <edinkarakurt99@gmail.com>
2018-04-13 07:29:32 +0300

    gitflow-feature-stash: TimerandRestart

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerScript : MonoBehaviour
+{
+    public TakeDamage Player_1_HP;
+    public TakeDamage Player_2_HP;
+    public float Timer = 60;
+    public Text timerText;
+
+    // Use this for initialization
+    void Start()
+    {
+        Player_1_HP = GameObject.Find("Player1").GetComponent<TakeDamage>();
+        Player_2_HP = GameObject.Find("Player2").GetComponent<TakeDamage>();
+        timerText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        timerText.text = Timer.ToString("f0");
+        print(Timer);
+        if (Timer <= 0.0f)
+        {
+            Timer = +60;
+        }
+        if (Player_1_HP.health == 0)
+        {
+            Player_1_HP.health = +11;
+            Player_2_HP.health = +11;
+            Timer = +60;
+        }
+        else if (Player_2_HP.health == 0)
+        {
+            Player_1_HP.health = +11;
+            Player_2_HP.health = +11;
+            Timer = +60;
+        }
+    }
+}
