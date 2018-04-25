59479ac8145c071f68e139819c5c2061df077c75
Unknown <fireblaze123@gmail.com>
2018-04-25 14:48:50 +0200

    gitflow-feature-stash: MeshLoad

---
+++ Assets/Scripts/MeshCustomization.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Anima2D;
+
+public class MeshCustomization : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+    public void ChangeMesh(int index)
+    {
+        GameObject child = transform.Find("Meshes").gameObject;
+        SpriteMeshAnimation[] meshes = child.GetComponentsInChildren<SpriteMeshAnimation>();
+        foreach (SpriteMeshAnimation mesh in meshes)
+        {
+            mesh.frame = index;
+        }
+    }
+}
