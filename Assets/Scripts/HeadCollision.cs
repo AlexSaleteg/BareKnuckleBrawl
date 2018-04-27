abc37bbcd1e4c9a8066c69e95d58ee4a8173f024
Alex S <alextsaleteg@gmail.com>
2018-04-27 11:20:59 +0200

    big stuff

--- Assets/Scripts/HeadCollision.cs
+++ Assets/Scripts/HeadCollision.cs
@@ -2,27 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeadCollision : MonoBehaviour {
+public class HeadCollision : MonoBehaviour
+{
 
     public string player;
-
     private TakeDamage damage;
+    private Animator animator;
+    private AnimationScript time;
     void Start()
     {
+        time = GameObject.Find(player).GetComponent<AnimationScript>();
+        animator = GameObject.Find(player).GetComponent<Animator>();
         damage = GameObject.Find(player).GetComponent<TakeDamage>();
     }
 
-   
+    //void Update()
+    //{
+    //    time.timeLeft -= Time.deltaTime;
+    //}
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "LightFist")
         {
-            damage.InflictDamage(10);
+            if (time.timeLeft >= 0.1f)
+            {
+                damage.InflictDamage(10);
+                time.timeLeft = +0.5f;
+                //animator.SetInteger("AnimState", 4);
+            }
+            else
+            {
+                damage.InflictDamage(5);
+                time.timeLeft = +0.5f;
+               // animator.SetInteger("AnimState", 4);
+            }
         }
         else if (other.gameObject.tag == "HeavyFist")
         {
-            damage.InflictDamage(20);
+            if (time.timeLeft >= 0.1f)
+            {
+                damage.InflictDamage(20);
+                time.timeLeft = +0.5f;
+                //animator.SetInteger("AnimState", 4);
+            }
+            else
+            {
+                damage.InflictDamage(10);
+                time.timeLeft = +0.5f;
+                //animator.SetInteger("AnimState", 4);
+            }
         }
+        if (time.chargeTimer >= 10)
+        {
+           damage.InflictDamage(30);
+           time.chargeTimer = 0;
+        }
+        //if (time.timeLeft <= 0.1f)
+        //{
+        //    animator.SetInteger("AnimState", 0);
+        //}
     }
 }
+
--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -6,73 +6,111 @@ using UnityEngine.SceneManagement;
 
 public class TimerScript : MonoBehaviour
 {
-    int playerWins =0;
-    static int Player_1_Win = 0;
-    static int Player_2_Win = 0;
+    public float player1Wins = 0;
+    public float player2Wins = 0;
+    private static int Player_1_Win = 0;
+    private static int Player_2_Win = 0;
     private float Timer = 60;
     public float PauseTimer = 0;
     private Text timerText;
     private TakeDamage Player_1_HP;
     private TakeDamage Player_2_HP;
+    public Slider Player_1_MaxHP;
+    public Slider Player_2_MaxHP;
+    public GameObject canvas;
+    public GameObject canvas2;
+    public GameObject canvas3;
+    public GameObject canvas4;
+    public Slider Player_1_HP_Slider;
+    public Slider Player_2_HP_Slider;
 
+    // Use this for initialization
     void Start()
     {
         timerText = GetComponent<Text>();
         Player_1_HP = GameObject.Find("Player1").GetComponent<TakeDamage>();
         Player_2_HP = GameObject.Find("Player2").GetComponent<TakeDamage>();
     }
-
+    // Update is called once per frame
     void Update()
     {
-
         Timer -= Time.deltaTime;
         timerText.text = Timer.ToString("f0");
-        if (Timer == 0)
+
+        if (Timer <= 0.001)
         {
-            SceneManager.LoadScene("Round");
+            if (Player_1_HP.health > Player_2_HP.health)
+            {
+                Player_1_Win = Player_1_Win + 1;
+                print("Player 1 wins a ROUND");
+                SceneManager.LoadScene("Workspace (Alex)");
+                canvas.SetActive(true);
+            }
+            else if (Player_2_HP.health > Player_1_HP.health)
+            {
+                Player_2_Win = Player_2_Win + 1;
+                print("Player 2 wins a ROUND");
+                SceneManager.LoadScene("Workspace (Alex)");
+                canvas2.SetActive(true);
+            }
         }
-        else if (Player_1_HP.health == 0 || Input.GetKeyDown(KeyCode.K))
+        if (Player_1_HP.health <= 0.001f)
         {
-            playerWins = 1;
-            Player_2_Win = Player_2_Win + 1;
-            print("Player 2 wins a ROUND");
+            player2Wins += Time.deltaTime;
+            PauseTimer += Time.deltaTime;
+            canvas2.SetActive(true);
         }
-        else if (Player_2_HP.health == 0 || Input.GetKeyDown(KeyCode.L))
+        else if (Player_2_HP.health <= 0.001)
         {
-            playerWins = 1;
-            Player_1_Win = Player_1_Win + 1;
-            print("Player 1 wins a ROOUND");
+            player1Wins += Time.deltaTime;
+            PauseTimer += Time.deltaTime;
+            canvas.SetActive(true);
         }
         if (Player_2_Win == 2)
         {
             print("Player 2 wins the MATCH");
-            PlayerPrefs.DeleteAll();
-
+            canvas4.SetActive(true);
         }
-        else if (Player_1_Win == 2)
+        if (Player_1_Win == 2)
         {
             print("Player 1 wins the MATCH");
-            PlayerPrefs.DeleteAll();
-
+            canvas3.SetActive(true);
         }
-        if (playerWins == 1)
+        if (player2Wins >= 5)
         {
-            PauseTimer += Time.deltaTime;   
+            Player_2_Win = Player_2_Win + 1;
+            print("Player 2 wins a ROUND");
         }
- 
-        if (Player_1_Win== 1 & PauseTimer >= 5 || Player_2_Win == 1 & PauseTimer >= 5)
+        if (player1Wins >= 5)
         {
-            print("puff");
-            SceneManager.LoadScene("Round");
-
+            Player_1_Win = Player_1_Win + 1;
+            print("Player 1 wins a ROUND");
+        } 
+        if (player1Wins >= 5 || player2Wins >= 5)
+        {
+            player2Wins = Time.deltaTime;
+            player1Wins = Time.deltaTime;
         }
-        if (Player_1_Win == 2 & PauseTimer >= 5 || Player_2_Win == 2 & PauseTimer >= 5)
+        if (PauseTimer >= 5 && Player_2_Win == 1 && Player_1_Win != 2)
         {
-            print("puff");
+            SceneManager.LoadScene("Workspace (Alex)");
+        }
+        if (PauseTimer >= 5 && Player_1_Win == 1 && Player_2_Win != 2)
+        {
+            SceneManager.LoadScene("Workspace (Alex)");
+        }
+        if (Player_1_Win == 2 && PauseTimer >= 9)
+        {
             Player_2_Win = 0;
             Player_1_Win = 0;
-            SceneManager.LoadScene("Round");
-
+            SceneManager.LoadScene("Workspace (Alex)");
+        }
+        if (Player_2_Win == 2 && PauseTimer >= 9)
+        {
+            Player_2_Win = 0;
+            Player_1_Win = 0;
+            SceneManager.LoadScene("Workspace (Alex)");
         }
     }
 }
+
