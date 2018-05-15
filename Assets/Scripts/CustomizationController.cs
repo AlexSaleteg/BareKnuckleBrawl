40778c24b1b13b86a6b50b78997355cf3779e0ea
Unknown <fireblaze123@gmail.com>
2018-05-15 15:00:43 +0200

    Customization V: Random Boogaloo

--- Assets/Scripts/CustomizationController.cs
+++ Assets/Scripts/CustomizationController.cs
@@ -10,15 +10,19 @@ public class CustomizationController : MonoBehaviour {
     public PlayerVisualsData data;
     public string choose;
     public string next;
+    public string randomize;
 
     private MeshCustomization player;
-    private int skinColor = 0;
-    private int moustache = 1;
-    private int moustacheColor = 0;
-    private int phase = 0;
+    private int skinColor;
+    private int moustache;
+    private int moustacheColor;
+    private int phase;
 
     void Start()
     {
+        skinColor = PlayerPrefs.GetInt("Player" + playerM.gameObject.name[6] + "SkinColor", 0);
+        moustache = PlayerPrefs.GetInt("Player" + playerM.gameObject.name[6] + "Moustache", 1);
+        moustacheColor = PlayerPrefs.GetInt("Player" + playerM.gameObject.name[6] + "MoustacheColor", 0);
         int bodytype = PlayerPrefs.GetInt("Player" + playerM.gameObject.name[6] + "BodyType", 0);
         if (bodytype == 0)
         {
@@ -36,8 +40,26 @@ public class CustomizationController : MonoBehaviour {
 
     void Update ()
     {
-
-        if (Input.GetKeyDown(next))
+        if (Input.GetKeyDown(randomize))
+        {
+            player.gameObject.SetActive(false);
+            if (Random.Range(0, 2) == 0)
+            {
+                player = playerF;
+                data.SetBodyType(1);
+            }
+            else
+            {
+                player = playerM;
+                data.SetBodyType(0);
+            }
+            player.gameObject.SetActive(true);
+            skinColor = Random.Range(0, player.source.skinColors.Length);
+            moustache = Random.Range(1, 6);
+            moustacheColor = Random.Range(0, player.source.moustacheColors.Length);
+            SaveData();
+        }
+        else if (Input.GetKeyDown(next))
         {
             phase++;
             if (phase > 3)
@@ -45,7 +67,7 @@ public class CustomizationController : MonoBehaviour {
                 phase = 0;
             }
         }
-        if (Input.GetKeyDown(choose))
+        else if (Input.GetKeyDown(choose))
         {
             if (phase == 0)
             {
@@ -69,9 +91,6 @@ public class CustomizationController : MonoBehaviour {
                 {
                     skinColor = 0;
                 }
-                data.SetSkinColor(skinColor);
-                player.RecolorSkin(skinColor);
-                player.RecolorMoustache(moustacheColor);
             }
             else if (phase == 2)
             {
@@ -80,9 +99,6 @@ public class CustomizationController : MonoBehaviour {
                 {
                     moustache = 1;
                 }
-                data.SetMoustache(moustache);
-                player.ChangeMoustache(moustache);
-                player.RecolorMoustache(moustacheColor);
             }
             else if (phase == 3)
             {
@@ -91,11 +107,19 @@ public class CustomizationController : MonoBehaviour {
                 {
                     moustacheColor = 0;
                 }
-                data.SetMoustacheColor(moustacheColor);
-                player.RecolorMoustache(moustacheColor);
             }
-
+            SaveData();
 
         }
 	}
+
+    void SaveData()
+    {
+        data.SetSkinColor(skinColor);
+        data.SetMoustache(moustache);
+        data.SetMoustacheColor(moustacheColor);
+        player.RecolorSkin(skinColor);
+        player.ChangeMoustache(moustache);
+        player.RecolorMoustache(moustacheColor);
+    }
 }
