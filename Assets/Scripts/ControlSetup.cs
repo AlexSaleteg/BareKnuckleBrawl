a28ce9803eabaa3910a56101d21215bdc684448a
Unknown <fireblaze123@gmail.com>
2018-05-23 11:04:23 +0200

    gitflow-feature-stash: ControlRework

---
+++ Assets/Scripts/ControlSetup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ControlScheme", menuName = "Control Scheme", order = 1)]
+public class ControlSetup : ScriptableObject {
+
+    public string Lfeint;
+    public string Rfeint;
+    public string lightAttack;
+    public string heavyAttack;
+    public string Defensive;
+    public string dodge;
+}
